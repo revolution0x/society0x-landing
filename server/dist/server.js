5793f4a514dbc3ccc113e6cda075cd979d6c13d9
Jay Welsh <thealchemicalopus@gmail.com>
2019-06-12 20:10:10 +0200

    added express server to serve gatsby build

---
+++ server/dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var path = require("path");
+var gatsyExpress = require('gatsby-plugin-express');
+var app = express();
+app.use(express.static(path.join(__dirname, '../../public/')));
+app.use(gatsyExpress('../../gatsby-express.json', {
+    publicDir: 'public/',
+    template: 'public/404/index.html',
+    // redirects all /path/ to /path
+    // should be used with gatsby-plugin-remove-trailing-slashes
+    redirectSlashes: true,
+}));
+var port = 3000;
+app.listen(port);
+console.log('App is listening on port ' + port);
---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import * as express from 'express';
+import * as path from 'path';
+const gatsyExpress = require('gatsby-plugin-express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../../public/')));
+app.use(gatsyExpress('../../gatsby-express.json', {
+    publicDir: 'public/',
+    template: 'public/404/index.html',
+  
+    // redirects all /path/ to /path
+    // should be used with gatsby-plugin-remove-trailing-slashes
+    redirectSlashes: true,
+}));
+
+const port = 3000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
